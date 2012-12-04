021ae25259897a67a74f569094b958aa2fafd8fb
Derek Johnson <derekj212@gmail.com>
2012-12-03 18:57:23 -0800

    First checkin
    Adaptation of other code--will use as a basis to repurpose

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easyeval::Application.config.secret_token = 'ed9ceff349cc633e36332b0df2a1f527934306f0b040f05f4e8224540fc59c50e34035ade71712fbcdd2da8b8d7d777d8d8251645e1aafc43df3255aff1e8870'
