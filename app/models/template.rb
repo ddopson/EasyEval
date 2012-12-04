a0c915a7278f058c3f4a711a22451b53cfca2ab4
Dave Dopson <ddopson@gmail.com>
2012-12-03 23:26:11 -0800

    use rubyzip instead of relying on cli (fails in heroku)

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,9 +1,32 @@
+require 'zip/zip'
+
 class Template
+  def self.unzip_file(zipfile, dest)
+    Zip::ZipFile.open(zipfile) { |entries|
+      entries.each { |f|
+        f_path = File.join(dest, f.name)
+        FileUtils.mkdir_p(File.dirname(f_path))
+        entries.extract(f, f_path)
+      }
+    }
+  end
+  def self.zip_dir(zipfile, dir)
+    Zip::ZipFile.open(zipfile, Zip::ZipFile::CREATE) do |entries|
+      input_filenames = Dir.glob("#{dir}/**/*", File::FNM_DOTMATCH).reject {|fn| File.directory?(fn) }
+      input_filenames.each do |filename|
+        entryname = filename.gsub("#{dir}/", '')
+        Rails.logger.info "TEMPLATE: Adding '#{entryname}' to zipfile"
+        entries.add(entryname, filename)
+      end
+    end
+  end
+
   def self.generate(value)
     template_file = "#{Rails.root}/templates/hello-world.docx"
     tmpdir=Dir.mktmpdir("generate")
     Rails.logger.info "TEMPLATE: Unzipping word doc '#{template_file}' into '#{tmpdir}'"
-    `unzip "#{template_file}" -d "#{tmpdir}"`
+    unzip_file template_file, tmpdir
+    #`unzip "#{template_file}" -d "#{tmpdir}"`
     Rails.logger.info "TEMPLATE: reading '#{tmpdir}/word/document.xml'"
     contents = File.read("#{tmpdir}/word/document.xml")
     Rails.logger.info "TEMPLATE: read #{contents.size} bytes from '#{tmpdir}/word/document.xml'"
@@ -11,9 +34,11 @@ class Template
     contents = contents.gsub(/VARIABLE_NAME/, value)
     Rails.logger.info "TEMPLATE: writing #{contents.size} bytes into '#{tmpdir}/word/document.xml'"
     File.write("#{tmpdir}/word/document.xml", contents)
-    Rails.logger.info "TEMPLATE: creating '#{tmpdir}/output.docx'"
-    `cd "#{tmpdir}"; zip -r output.docx *`
-    Rails.logger.info "TEMPLATE: returning '#{tmpdir}/output.docx'"
+    output_file = "#{tmpdir}/output.docx"
+    Rails.logger.info "TEMPLATE: creating '#{output_file}'"
+    zip_dir output_file, tmpdir
+    #`cd "#{tmpdir}"; zip -r output.docx *`
+    Rails.logger.info "TEMPLATE: returning '#{output_file}'"
     return "#{tmpdir}/output.docx"
   end
 end
