79d7a96914ca6aba772351273a93a7d45a7ab34c
Dave Dopson <ddopson@gmail.com>
2013-01-27 14:58:07 -0800

    refac: split out checkbox-rollup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,12 @@
 //= require bootstrap
 //= require underscore
 //= require backbone
+//= require ./vendor/jquery-ui-1.10.0.custom
+//= require ./vendor/jquery-uicombobox
+//= require ./vendor/jquery.externalscript
+//= require ./vendor/jquery.readyselector
+//= require ./vendor/twitter-widgets
+//= require_tree ./vendor
 //= require tilt-jade/runtime
 //= require_self
 //= require_tree .
