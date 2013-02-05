9440f7094621dc8bf72d860934ece076512d2de2
Dave Dopson <ddopson@gmail.com>
2013-02-04 22:21:19 -0800

    add Micaela page

--- app/controllers/template_controller.rb
+++ app/controllers/template_controller.rb
@@ -5,6 +5,28 @@ class TemplateController < ApplicationController
   def editor
   end
 
+  def micaela
+    raise "Forbidden" unless Rails.env.development?
+  end
+
+  def micaela_submit
+    raise "Forbidden" unless Rails.env.development?
+    txt = params['_doc.slim']
+    unless txt.length > 200
+      raise "_doc.slim is REALLY short. only #{_doc.slim.length} chars"
+    end
+    i = Time.now.to_i
+    path = "#{Rails.root}/app/views/template/_doc.slim"
+    File.write("#{path}.#{i}", txt)
+    if File.symlink?(path)
+      FileUtils.rm(path)
+    else
+      FileUtils.mv(path, "#{path}.bak.#{i}")
+    end
+    File.symlink("#{path}.#{i}", path)
+    redirect_to '/template'
+  end
+
   def generate
     if params[:JSON_ONLY]
       render :json => params
